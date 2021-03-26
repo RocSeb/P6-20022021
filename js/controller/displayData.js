31469e679aad2b1154f6728e67e1261da8a73cb2
Sebastien Rocher <srocher@live.fr>
2021-03-26 11:54:11 +0100

    remove filter

--- js/controller/displayData.js
+++ js/controller/displayData.js
@@ -9,8 +9,6 @@ function getJson(){
         displayData = dataJson;
         displayPhotographers(dataJson);  
         displayByDefault(dataJson)                       
-        //filterTag(dataJson)       
-        //redirectAndFilter(dataJson)
     })
 };
 getJson();
@@ -40,76 +38,12 @@ function displayByDefault(dataJson) {
 };
 
 // Injecte le DOM dans la vue
+
 function displayPhotographers(dataJson){
-  const buttons = document.querySelectorAll(".filtres span");
-  buttons.forEach(btn => btn.addEventListener("click", () => {  
-    const photographersDiv = document.getElementById('photographes_container');
-    photographersDiv.innerHTML = "";                        
-  }));
+  const photographersDiv = document.getElementById('photographes_container');
+  photographersDiv.innerHTML = "";                        
 }; 
 
 
 
 
-/****
- * processus du filtre:
- *    l'utilisateur clique sur le tag (fonction a executé qui filtre => event click)
- *    affiche les profiles avec le meme tag
- ****/
-
-/****
- * activation bouton filtre
- *    isTagActive = false;
- *    function event click sur le bouton tag du filtre
- *      if isTagActive === true {
- *        isTagActive = false;
- *      } else {
- *        isTagActive = true; 
- *      }
- ****/ 
- 
-
-
-
-
-
-
-
-
-/*
-// Filtre 
-const tagName = window.location.search.split('id=')[1];  
-function redirectAndFilter(dataJson) {
-  if(tagName){
-    const photographersDiv = document.getElementById('photographes_container');
-    photographersDiv.innerHTML = "";  
-    filterElements(dataJson, tagName)
-  }
-} 
-
-
-// Fonction ACTIVE en clickant sur le bouton 
-
-function addActiveClass() {
-  const buttons = document.querySelectorAll(".filtres span");
-  buttons.forEach(btn => btn.addEventListener("click", () => {
-    buttons.forEach(btn => btn.classList.remove('active'));
-    btn.classList.add('active'); 
-  }));
-}
-addActiveClass(); 
-
-// Ajoute un événement à chaque clicke //
-
-
- // Filtre par tags 
-
-function filterTag(dataJson) {  
-  document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('individual-tags')) {
-      const photographersDiv = document.getElementById('photographes_container');
-      photographersDiv.innerHTML = "";                                      
-      filterElements(dataJson, event.target);     // appel de la fonction
-    }
-  });
-}; */
