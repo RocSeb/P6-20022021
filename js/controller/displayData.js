1ae5a944023388ab2c855c1b4b5d02d1b23a6b98
Sebastien Rocher <srocher@live.fr>
2021-05-01 15:21:11 +0200

    new filter

--- js/controller/displayData.js
+++ js/controller/displayData.js
@@ -1,5 +1,8 @@
 //Variables
-let isActiveClass;
+let isActiveClass = "true";
+const tabs = document.querySelectorAll(".filtres span");
+const all = document.querySelectorAll(".photographerContainer");
+let tagPerso;
 // Methode Fetch
 let getJson = () => {
   fetch('./../../data/data.json')    //permet de recuperer les ressources
@@ -7,8 +10,9 @@ let getJson = () => {
     .then(dataJson => { 
         displayData = dataJson;
         displayPhotographers(dataJson);  
-        displayByDefault(dataJson);
-        filterActiveTag();          
+        displayByDefault(dataJson); 
+        scroll();
+        getFilterTag();
     })
 };
 getJson();
@@ -43,7 +47,7 @@ let displayPhotographers = () => {
     const photographersDiv = document.getElementById('photographes_container');
     photographersDiv.innerHTML = "";
 }; 
-
+/*
 let filterActiveTag = () => {
  
   const tabs = document.querySelectorAll(".filtres span");
@@ -97,7 +101,6 @@ let filterActiveTag = () => {
                   events.style.display = "block";
                 })
               }
-              console.log(tabval);
             })
         } else {
          tab.classList.remove('active');
@@ -108,3 +111,43 @@ let filterActiveTag = () => {
     })
   })
 }
+*/
+
+const mybutton = document.getElementById("scroll_up");
+window.onscroll = function() {scroll()};
+
+let scroll = () =>  {
+  if (document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
+});
+
+
+
+
+
+/* j'ai une liste de tag en haut
+* chaque tag contiendra un attribut data-tabs
+* lorsque je clique sur un tag il affichera les profils qui contiendront le meme tag
+* lorsque je clique sur un nouveau tag, les profils qui auront ce nouveau tag s'ajouteront a ceux qui ont été affiché précédement
+* lorsque je clique une 2eme fois sur le tag actif, celui-ci se désactivera.
+* si aucun tag n'est sélectionné, ma page affichera tous les profils
+*/ 
+
+let getFilterTag = () => {
+  tabs.forEach(val => {
+    val.addEventListener("click", () => {
+      tagPerso = val;
+    })
+  })
+}  
+
+/* Recuperer chaque data-tabs
+*  Recuperer chaque tag par photographe ( doit etre selectionnable 1 par 1)
+*/
